0c4c21c42087c2c4a4c9a0cd55da7ca01952d922
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-19 10:17:54 +0500

    number of post month

--- get_post_month.py
+++ get_post_month.py
@@ -1,4 +1,5 @@
 from read_data import fromJson
+import datetime
 
 
 def get_post_month(data:dict,month:int)->int:
@@ -12,5 +13,18 @@ def get_post_month(data:dict,month:int)->int:
     Returns: 
         int: the number of posts for the given month
     """
-    
-    return
+    number_of_month_msg: int = 0
+    for post in data["messages"]:
+
+        if post["type"] == "message":
+            i = int(post["date_unixtime"])
+            time = datetime.datetime.fromtimestamp(i)
+            m = int(time.strftime("%m"))
+            
+            if m == month:
+                number_of_month_msg += 1
+
+    return number_of_month_msg
+
+data = fromJson('data/result.json')
+# print(get_post_month(data, 10))
